557c05927f492bfd4bf2119e7691bce75814cee7
Juliana Daprá <Juliana.dapra@gmail.com>
2025-10-02 21:10:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6567329290-b670c",
+  "appId": "1:345854422039:web:8ae236ab90091fe8f98052",
+  "apiKey": "AIzaSyBEN6bmDYdeFh3E-xQVVvqQqN-vJPZzsBY",
+  "authDomain": "studio-6567329290-b670c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "345854422039"
+};
